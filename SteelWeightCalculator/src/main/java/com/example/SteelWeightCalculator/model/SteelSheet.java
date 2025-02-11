9734117c490b15cd218f2a8738ebc0ebe1d98dc1
dimitar-st-stoyanov <dimitar.stoyanov2305@gmail.com>
2025-02-11 07:03:15 +0200

    Update SteelSheet.java

--- SteelWeightCalculator/src/main/java/com/example/SteelWeightCalculator/model/SteelSheet.java
+++ SteelWeightCalculator/src/main/java/com/example/SteelWeightCalculator/model/SteelSheet.java
@@ -38,5 +38,50 @@ public class SteelSheet {
 		this.thickness = thickness;
 	}
 	
+	public double getDensity(String type) {
+		double density = 0;
+		switch (type) {
+		case "304": density = 7.93; break;
+		case "316":
+		case "310":
+		case "317": density = 7.98; break;
+		case "321": density = 7.92; break;
+		case "17-4-PH":
+		case "409":
+		case "410":
+		case "420": density = 7.75; break;
+		case "430":
+		case "439": density = 7.70; break;
+		case "444":
+		case "440C":
+		case "2205": density = 7.80; break;
+		case "2507": density = 7.85; break;
+		case "Carbon Steel": density = 7.85; break;
+		
+		default: System.out.println("No such as type"); break;
+		}
+		return density;
+	}
 	
 }
+
+/*1. Austenitic Stainless Steels
+304 → 7.93 g/cm³//
+316 → 7.98 g/cm³//
+321 → 7.92 g/cm³//
+310 → 7.98 g/cm³//
+317 → 7.98 g/cm³//
+2. Ferritic Stainless Steels
+409 → 7.75 g/cm³//
+430 → 7.70 g/cm³//
+439 → 7.70 g/cm³//
+444 → 7.80 g/cm³
+3. Martensitic Stainless Steels
+410 → 7.75 g/cm³//
+420 → 7.75 g/cm³//
+440C → 7.80 g/cm³
+4. Duplex Stainless Steels
+2205 → 7.80 g/cm³
+2507 → 7.85 g/cm³
+5. Precipitation Hardening (PH) Stainless Steels
+17-4 PH → 7.75 g/cm³*/
