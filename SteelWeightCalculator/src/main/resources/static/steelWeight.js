8e6108561c7e2a154cb26c975492d90bc28457ff
dimitar-st-stoyanov <dimitar.stoyanov2305@gmail.com>
2025-02-20 22:15:19 +0200

    small update

--- SteelWeightCalculator/src/main/resources/static/steelWeight.js
+++ SteelWeightCalculator/src/main/resources/static/steelWeight.js
@@ -3,6 +3,11 @@ function calculate(){
 	const thickness = document.getElementById("thickness").value;
 	const length = document.getElementById("length").value;
 	const width = document.getElementById("width").value;
+	const resultContainer = document.getElementById("result");
+	const loadingSpinner = document.getElementById("loading");
+	
+	resultContainer.innerText = "";
+	   resultContainer.classList.remove("error");
 	
 	if(!thickness||!length||!width||type==""){
 		alert('Please fill all fields');
@@ -10,7 +15,9 @@ function calculate(){
 	}
 	
 	let url = `http://localhost:7979/steel-weight-calculator/calculate-weight?type=${encodeURIComponent(type)}&length=${length}&width=${width}&thickness=${thickness}`;
-		
+	
+	loadingSpinner.classList.remove("hidden");
+	
 	fetch( url,{
 		method : "GET",
 		headers : {
@@ -19,7 +26,11 @@ function calculate(){
 	})
 	.then(response => response.json())
 	.then(data => {
-	               document.getElementById('result').innerText = `Weight: ${data.toFixed(2)} kg.`;
+			loadingSpinner.classList.add("hidden");
+
+		        // Display calculated weight
+		        resultContainer.innerText = `🔹 Weight: ${data.toFixed(2)} kg`;
+	              
 	           })
 	           .catch(err => {
 	               document.getElementById('result').innerText = "Error: " + err.message;
