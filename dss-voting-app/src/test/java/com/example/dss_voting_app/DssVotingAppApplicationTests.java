034af2f80bebd41cc88b6fca51e419f0c2266983
dimitar-st-stoyanov <dimitar.stoyanov2305@gmail.com>
2025-04-05 16:58:42 +0300

    New Project

---
+++ dss-voting-app/src/test/java/com/example/dss_voting_app/DssVotingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss_voting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssVotingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
