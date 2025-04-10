090df9e08dc0c45365dd7be960133cb76183f47b
dimitar-st-stoyanov <dimitar.stoyanov2305@gmail.com>
2025-04-10 07:15:07 +0300

    New Project

---
+++ dss-ecom-app/dss-ecom-app/src/main/java/com/ecomm/project/DssEcomAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssEcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssEcomAppApplication.class, args);
+	}
+
+}
---
+++ dss-ecom-app/dss-ecom-app/src/test/java/com/ecomm/project/DssEcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssEcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
