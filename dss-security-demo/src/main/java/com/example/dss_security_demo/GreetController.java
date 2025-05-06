0b3382d68eade62b7af1774e5dc3b1b43bc0792d
dimitar-st-stoyanov <dimitar.stoyanov2305@gmail.com>
2025-05-06 08:28:10 +0300

    Implements Security

--- dss-security-demo/src/main/java/com/example/dss_security_demo/GreetController.java
+++ dss-security-demo/src/main/java/com/example/dss_security_demo/GreetController.java
@@ -1,5 +1,6 @@
 package com.example.dss_security_demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,17 @@ public class GreetController {
     public String sayHello(){
         return "Hello";
     }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "Hello, Admin!";
+    }
+
 }
